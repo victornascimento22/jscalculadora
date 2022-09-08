8bee74ba5533760685d22ec0a13357799614ba13
victor nascimento <nascimentovictor22@gmail.com>
2022-09-08 20:21:43 -0300

    att

---
+++ app/Index.js
@@ -0,0 +1,49 @@
+﻿function onClickSalvar() {
+
+    var resp = document.getElementById('resp');
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+    var texto = '';
+
+
+    if (documento.getElementById('soma').checked)
+        texto = soma(num1, num2);
+    if (documento.getElementById('subtracao').checked)
+        texto = subtracao(num1, num2);
+    if (document.getElementById('multiplicacao').checked)
+        texto = multiplicacao(num1, num2);
+    if (documento.getElementById('divisao').checked)
+        texto = divisao(num1, num2);
+
+    alert('resp');
+  
+
+
+    function soma(x, y) {
+
+        return (x + y);
+    }
+
+    function subtracao(x, y) {
+
+        return (x - y);
+
+
+    }
+
+    function multiplicacao(x, y) {
+
+        return (x * y);
+
+
+    }
+
+    function divisao(x, y) {
+
+
+        return (x / y);
+
+    }
+
+}
